cf648ca6305b1bf68d1b38976a96bf3ead444bd5
raymok1 <raymok1@mcmaster.ca>
2025-03-29 16:15:30 -0400

    Connect to TheMovieDb web API using wrapper

--- project.py
+++ project.py
@@ -1,15 +1,20 @@
-import redis
+# import redis
 
-r = redis.Redis(
-    host='redis-16105.c323.us-east-1-2.ec2.redns.redis-cloud.com',
-    port=16105,
-    decode_responses=True,
-    username="default",
-    password="Uyh3GwUscxJ6f7ivzCNERzYZo8OM7ep0",
-)
+# r = redis.Redis(
+#     host='redis-16105.c323.us-east-1-2.ec2.redns.redis-cloud.com',
+#     port=16105,
+#     decode_responses=True,
+#     username="default",
+#     password="Uyh3GwUscxJ6f7ivzCNERzYZo8OM7ep0",
+# )
 
-success = r.set('name', 'kayla')
-print(success)
+# success = r.set('name', 'kayla')
+# print(success)
 
-result = r.get('name')
-print(result)
+# result = r.get('name')
+# print(result)
+
+import tmdbsimple as tmdb
+tmdb.API_KEY = '854604cc4fd32fce5035a3fd5f61cfd7'
+movie = tmdb.Movies(603)
+print(movie.info())
