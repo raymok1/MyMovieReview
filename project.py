e77e7c5c3f53c80292cf71c7a5fd3ca656e917fe
raymok1 <raymok1@mcmaster.ca>
2025-03-29 17:29:31 -0400

    Implement movie search feature

--- project.py
+++ project.py
@@ -3,18 +3,15 @@ import tmdbsimple as tmdb
 
 # Redis DB connection
 r = redis.Redis(
-    host='redis-16105.c323.us-east-1-2.ec2.redns.redis-cloud.com',
-    port=16105,
-    decode_responses=True,
-    username="default",
-    password="Uyh3GwUscxJ6f7ivzCNERzYZo8OM7ep0",
+    host = "redis-16105.c323.us-east-1-2.ec2.redns.redis-cloud.com",
+    port = 16105,
+    decode_responses = True,
+    username = "default",
+    password = "Uyh3GwUscxJ6f7ivzCNERzYZo8OM7ep0",
 )
 
-
 # TheMovieDb web API connection via tmdbsimple wrapper: https://github.com/celiao/tmdbsimple/
-# tmdb.API_KEY = '854604cc4fd32fce5035a3fd5f61cfd7'
-# movie = tmdb.Movies(603)
-# print(movie.info())
+tmdb.API_KEY = "854604cc4fd32fce5035a3fd5f61cfd7"
 
 # Login
 login = False
@@ -32,5 +29,11 @@ while login == False:
     if login == False:
         print("Incorrect username or password. Please try again.")
 
-
 print("LOGGED in")
+
+# Search for movie
+keyword = input("Search for a movie by title or keyword: ")
+search = tmdb.Search().movie(query = keyword)
+print("Id\tTitle\t\tRelease Date")
+for s in search["results"]:
+    print(f"{s["id"]}\t{s["title"]}\t{s["release_date"]}")
