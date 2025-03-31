9913eafb88e1f9daf2974824e5f16fe401a3a153
raymok1 <raymok1@mcmaster.ca>
2025-03-31 10:35:16 -0400

    ERROR: exception when fetching movies from API

--- project.py
+++ project.py
@@ -14,26 +14,46 @@ r = redis.Redis(
 tmdb.API_KEY = "854604cc4fd32fce5035a3fd5f61cfd7"
 
 # Login
-login = False
-print("Please login")
-while login == False:
-    username = input("Username: ")
-    password = input("Password: ")
+# login = False
+# print("Please login")
+# while login == False:
+#     username = input("Username: ")
+#     password = input("Password: ")
 
-    db_username = r.get("username")
-    db_password = r.get("password")
+#     db_username = r.get("username")
+#     db_password = r.get("password")
 
-    if username == db_username and password == db_password:
-        login = True
+#     if username == db_username and password == db_password:
+#         login = True
 
-    if login == False:
-        print("Incorrect username or password. Please try again.")
+#     if login == False:
+#         print("Incorrect username or password. Please try again.")
 
-print("LOGGED in")
+# print("LOGGED in")
 
 # Search for movie
-keyword = input("Search for a movie by title or keyword: ")
-search = tmdb.Search().movie(query = keyword)
+# keyword = input("Search for a movie by title or keyword: ")
+# search = tmdb.Search().movie(query = keyword)
+# print("Id\tTitle\t\tRelease Date")
+# for s in search["results"]:
+#     print(f"{s["id"]}\t{s["title"]}\t{s["release_date"]}")
+
+# Add movie to watchlist
+# movie_id = input("Input the id of the movie you wish to select: ")
+# movie = tmdb.Movies(movie_id).info()
+# print(f"You have selected '{movie["title"]}'")
+# add_to_watchlist = input("Would you like to add this movie to your watchlist? Y or N: ")
+
+# if add_to_watchlist == "Y" or add_to_watchlist == "y":
+#     print("add movie to watchlist")
+# else:
+#     print("go back to main menu")
+
+watchlist = r.smembers("watchlist")
+print(watchlist)
+
 print("Id\tTitle\t\tRelease Date")
-for s in search["results"]:
-    print(f"{s["id"]}\t{s["title"]}\t{s["release_date"]}")
+for movie_id in watchlist:
+    movie = tmdb.Movies(movie_id).info()
+    print(f"{movie["id"]}\t{movie["title"]}\t{movie["release_date"]}")
+
